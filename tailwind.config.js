25d64430138d9960a1841a3b1d88b34e95d6ff6c
Deepanshu <deepanshu@befisc.com>
2026-05-16 14:06:31 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}", // For App Router
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}", // For Pages Router
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}", // If using /src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
